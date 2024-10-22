7fe0df1e107c7fad7352e76c96b7ce98e457a7dd
Y-path <jno333@gmail.com>
2024-10-21 21:26:43 -0400

    fifth commit

--- index2.js
+++ index2.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
@@ -5,25 +6,90 @@ const error = email.nextElementSibling;
 const name = document.getElementById("name");
 const h1 = document.createElement("h1");
 h1.textContent = "Artist Discovery";
-document.body.prepend(h1);
 h1.style.color = "white";
 h1.style.fontSize = "40px";
+h1.style.margin = "auto";
+h1.style.marginBottom = "30px";
+document.body.append(h1);
+
 const h2 = document.createElement("h2");
 h2.style.color = "white";
 h2.style.fontSize = "12px";
-h2.textContent = "Click on the image below"
-h1.append(h2)
-const image = new Image();
-const imageUrl = "https://uploads2.wikiart.org/images/remedios-varo/the-call.jpg!Large.jpg";
-image.src = imageUrl;
-image.style.margin = "20px";
-image.title = "The Call by Remedios Varo";
+h2.textContent = "Click on the image below";
+h2.style.margin = "auto";
+h2.style.marginBottom = "30px";
+document.body.append(h2);
 
-h2.append(image);
+const image = document.createElement("image");
+document.body.append(image);
 
-const body = document.body;
+form.style.margin = "auto";
+form.style.marginBottom = "30px";
+document.body.append(form);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Upcoming Featured Artists";
+h3.style.color = "white";
+h3.style.fontSize = "20px";
+h3.style.margin = "auto";
+h3.style.marginBottom = "20px";
+document.body.append(h3);
+
+document.getElementById("list")
+list.style.width = "180px";
+list.style.margin = "auto";
+list.style.color = "white"
+list.style.marginBottom = "40px"
+document.body.append(list)
+const span = document.createElement("span")
 
 
+let imagesArray = [
+    {
+        title: "The Call by Remedios Varo",
+        url: "https://uploads2.wikiart.org/images/remedios-varo/the-call.jpg!Large.jpg"
+    },
+    {
+        title: "The Gathering",
+        url: "https://uploads7.wikiart.org/images/remedios-varo/the-gathering.jpg!Large.jpg"
+    },
+    {
+        title: "Character by Remedios Varo",
+        url: "https://uploads3.wikiart.org/images/remedios-varo/character.jpg!Large.jpg"
+    }
+];
+ 
+function displayRandomImage() {
+    const randomIndex = Math.floor(Math.random() * imagesArray.length);
+    const randomImage = imagesArray[randomIndex].url;
+    const randomTitle = imagesArray[randomIndex].title; 
+
+    const imageEl = document.createElement("img");
+    imageEl.src = randomImage;
+    imageEl.title = randomTitle;
+    imageEl.style.margin = "auto";
+    imageEl.style.marginTop = "15px";
+    
+    imageEl.onclick = function() {
+        window.open("https://www.wikiart.org/en/remedios-varo", "_blank");;
+      };
+    //   document.body.append(imageEl);
+   h2.append(imageEl);
+}
+ window.onload = displayRandomImage;
+
+
+const artists = ["Vincent Van Gogh", "Mark Rothko", "Egon Schiele"];
+
+const fragment = document.createDocumentFragment();
+
+for (let i in artists) {
+    const li = document.createElement("li");
+    li.textContent = artists[i];
+    fragment.appendChild(li);
+    
+}
+document.getElementById("list").appendChild(fragment);
 
 
 document.getElementById("h1");
@@ -31,9 +97,10 @@ h1.addEventListener("click", function() {
     h1.textContent = "Discover a New Artist";
     h1.style.color = "yellow";
     h1.style.fontFamily = "Roboto"
-    h1.append(h2);
-    h2.append(image);
+    h1.insertBefore(h2);
+   
 });
+
 
 h1.addEventListener("mouseover", function() {
     this.style.cursor = "pointer";
@@ -52,15 +119,9 @@ function changeBackgroundColor() {
   }  
   const intervalId = setInterval(changeBackgroundColor, 80);
 
-document.getElementById("image");
-image.addEventListener("click", function() {
-    window.open("https://www.wikiart.org/en/remedios-varo", "_blank");
-});
-
 
 const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
 
 window.addEventListener("load", () => {
   
@@ -70,6 +131,7 @@ window.addEventListener("load", () => {
 
 
 email.addEventListener("input", () => {
+    
   const isValid = email.value.length === 0 || emailRegExp.test(email.value);
   if (isValid) {
     email.className = "valid";
@@ -93,38 +155,47 @@ form.addEventListener("submit", (event) => {
     email.className = "valid";
     error.textContent = "";
     error.className = "error";
+    alert("Thank you. We will be in touch!");
+    document.getElementById("form").reset(); 
+  
   }
-});string
+ 
+});
+
 
 function checkName() {
     const nameValue = document.getElementById("name").value;
-   
-    if (nameValue === "") {
+     if (nameValue === "") {
       alert("Please enter your name.");
     }
 };
 
-function submitForm() {
-    alert("Thank you. We will be in touch!");
-    document.getElementById("form").reset();  
-  }
-
-const button = document.getElementById("button");
-button.setAttribute("name", "submitButton");
-button.setAttribute("disabled", "");
 
 
 
-// below does not work
+// All the stuff below does not work
+// I left it here to show all the work I put into this
 
+//  document.getElementById("image");
+// image.addEventListener("click", (event) => {
+//      event.preventDefault();
+//     window.open("https://www.wikiart.org/en/remedios-varo", "_blank");
+// });
+
+// function submitForm() {
+//     alert("Thank you. We will be in touch!");
+//     document.getElementById("form").reset();  
+//   }
+
+// const button = document.getElementById("button");
+// button.setAttribute("name", "submitButton");
+// button.setAttribute("disabled", "");
 
 // const form2 = document.createElement("form2");
 // formSubmit.classList.add("hidden")
 // formSubmit.textContent = "Come back tomorrow for a new artist";
 // image.append(formSubmit); 
 // document.getElementsByClassName("hidden")
-
-
 
 // function changeImage(x, image) {
 //     if(x==1) {
@@ -134,9 +205,6 @@ button.setAttribute("disabled", "");
 //         image.src = "https://uploads6.wikiart.org/images/remedios-varo/creation-of-the-birds.jpg!Large.jpg";
 //     }
 //     }
-
-
-
 
 // document.getElementById("image");
 // image.addEventListener("mouseover", () => {
@@ -153,38 +221,16 @@ button.setAttribute("disabled", "");
 
 // });
 
-
-
-
-
-
-
 //  document.querySelector("form")
 //   form.addEventListener("mouseover", () => {
 //     form.setAttribute("style", "background-color: lightblue;");
 
 //   });
 
-
-
-
-
-
-
-
-
 //   document.getElementById("submit");
 //   submit.addEventListener("click", () => {
 //     submit.setAttribute("data-text", "Submitted!");
 //   });
-
- 
- 
-
-
-
-
-
 
 // document.getElementById("form2");
 // form.addEventListener("submit", (event) => {
@@ -193,10 +239,6 @@ button.setAttribute("disabled", "");
 //     form2.classList.remove("hidden");
 // });
 
-
-// below does not work
- 
- 
 //  form.addEventListener("submit", (event) => {
 //     event.preventDefault();
 //     form.classList.remove("active");
@@ -204,8 +246,6 @@ button.setAttribute("disabled", "");
 //     formSubmit.classList.remove("hidden");
 //     formSubmit.classList.add("active");
 //  });
-
-
 
 // form.addEventListener("submit", function(event) {
 //    event.preventDefault(); 
@@ -221,4 +261,3 @@ button.setAttribute("disabled", "");
 //     document.getElementsByClassName = "submitted"  
 // }
 
-// document.
